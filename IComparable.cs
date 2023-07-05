7a35dbaf415ec5efac61c42e25b54ee9badef116
Максимова Анастаися <imbq@gmail.com>
2023-07-05 11:50:15 +0300

    Добавьте файлы проекта.

---
+++ IComparable.cs
@@ -0,0 +1,10 @@
+﻿// IComparable.cs
+using System;
+
+namespace YourLastName.Library
+{
+    public interface IComparable
+    {
+        int CompareTo(object obj);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Program.cs
+using System;
+using System.Runtime.ConstrainedExecution;
+using NazarovaUlyana.Library;
+
+namespace NazarovaUlyana.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage garage = new Garage();
+            garage.Name = "Ulyana Garage";
+            garage.Address = "Tikhonravova 14";
+
+            Car car1 = new Car { Owner = "Vitali Ivanov", Number = "AA345H" };
+            Car car2 = new Car { Owner = "Irina Romanova", Number = "DJ567B" };
+            Car car3 = new Car { Owner = "Elena Smirnova", Number = "GR746K" };
+
+            garage.Cars.Add(car1);
+            garage.Cars.Add(car2);
+            garage.Cars.Add(car3);
+
+            garage.ShowAll();
+
+            Console.WriteLine("\nSorting cars by owner...");
+            garage.Cars.Sort();
+            garage.ShowAll();
+
+            Console.ReadLine();
+        }
+    }
+}
